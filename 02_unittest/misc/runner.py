e695887dc2c0214eec9b3abb38def542ef6a32a4
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-22 01:00:35 -0500

    Create runner.py

---
+++ 02_unittest/misc/runner.py
@@ -0,0 +1,12 @@
+import unittest
+import test_shapes
+import test_sizes
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+
+suite.addTests(loader.loadTestsFromModule(test_shapes))
+suite.addTests(loader.loadTestsFromModule(test_sizes))
+
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
