9fac95233c867b1eb08dd9166ba9370208278687
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-22 15:34:36 -0500

    Update circle_area.py

--- 04_pytest/circle/circle_area.py
+++ 04_pytest/circle/circle_area.py
@@ -1,8 +1,8 @@
 import math
 
 def circle_area(radius):
-  if (radius < 0):
-      raise ValueError("The radius cannot be negative.")
   if not type(radius) in [int, float]:
       raise TypeError("The radius is not an int or float.")
+  if (radius < 0):
+      raise ValueError("The radius cannot be negative.")
   return math.pi * radius**2
