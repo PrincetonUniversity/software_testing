3a579a71961f5d722efaf7d5271fddd029f5a7cf
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-22 00:59:11 -0500

    Create sizes.py

---
+++ 02_unittest/misc/sizes.py
@@ -0,0 +1,25 @@
+class Big:
+
+    def __init__(self, mysize):
+        self.mysize = mysize
+
+    def check(self):
+        return self.mysize > 75
+
+
+class Medium:
+
+    def __init__(self, mysize):
+        self.mysize = mysize
+
+    def check(self):
+        return 25 < self.mysize <= 75
+
+
+class Small:
+
+    def __init__(self, mysize):
+        self.mysize = mysize
+
+    def check(self):
+        return self.mysize <= 25
