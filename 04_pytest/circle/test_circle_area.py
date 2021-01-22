d79961c420135ed02341cec64cf91e72e801cbdf
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-22 12:10:52 -0500

    Update test_circle_area.py

--- 04_pytest/circle/test_circle_area.py
+++ 04_pytest/circle/test_circle_area.py
@@ -2,22 +2,24 @@ import pytest
 from circle_area import circle_area
 import math
 
-def test_area():
-    # test areas when radius >= 0
-    assert circle_area(1) == math.pi
-    assert circle_area(0) == 0
-    assert circle_area(2.1) == math.pi * 2.1**2
+class TestClass:
 
-def test_values():
-    # raise value error when radius is negative
-    with pytest.raises(ValueError):
-        circle_area(-2)
+    def test_area(self):
+        # test areas when radius >= 0
+        assert circle_area(1) == math.pi
+        assert circle_area(0) == 0
+        assert circle_area(2.1) == math.pi * 2.1**2
 
-def test_types():
-    # handle type errors
-    with pytest.raises(TypeError):
-        circle_area(3+5j)
-    with pytest.raises(TypeError):
-        circle_area(True)
-    with pytest.raises(TypeError):
-        circle_area("cat")
+    def test_values(self):
+        # raise value error when radius is negative
+        with pytest.raises(ValueError):
+            circle_area(-2)
+
+    def test_types(self):
+        # handle type errors
+        with pytest.raises(TypeError):
+            circle_area(3+5j)
+        with pytest.raises(TypeError):
+            circle_area(True)
+        with pytest.raises(TypeError):
+            circle_area("cat")
