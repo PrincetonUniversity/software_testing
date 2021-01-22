7ef1a871eb29af924ff28e4d8bebd62aa1a1800f
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-22 00:18:21 -0500

    Update circle_area.py

--- 02_unittest/circle_area.py
+++ 02_unittest/circle_area.py
@@ -1,7 +1,7 @@
 import math
 
 def circle_area(radius):
-  return math.pi * radius**2
+    return math.pi * radius**2
   
-  if __name__ == "__main__":
+if __name__ == "__main__":
     print("Success")
