0e3f8f54e063ed94aa000d1b1ee460bd3e3abd14
wh3248 <wh3248@princeton.edu>
2023-01-21 12:41:00 -0500

    add QR code and fix circle_area to raise exceptions

--- 02_unittest/circle_area.py
+++ 02_unittest/circle_area.py
@@ -2,6 +2,10 @@
 import math
 
 def circle_area(radius):
+    if not type(radius) in (int, float):
+        raise TypeError("The radius is not an int or float.")
+    if radius < 0:
+        raise ValueError("The radius cannot be negative.")    
     return math.pi * radius**2
   
 if __name__ == "__main__":
