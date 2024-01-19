a1c6a8e4438f6bc599bff65cbe8db1f7608d9db0
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-01-19 11:54:19 -0500

    Update test_func.py

--- 02_pytest/test_func.py
+++ 02_pytest/test_func.py
@@ -1,11 +1,4 @@
 from func import func
-import pytest
-import flask
-import requests
 
 def test_answer():
     assert func(3) == 4
-    print("Success!")
-
-if __name__ == "__main__":
-    test_answer()
